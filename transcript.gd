e202d31ec0f738f8f4e281fdbfd78670f1368bea
atreusph <maverickjohn4b@gmail.com>
2026-09-08 14:36:27 +0800

    Remove introductory comments from transcript.gd
    Removed introductory comments from transcript.gd.

--- transcript.gd
+++ transcript.gd
@@ -1,10 +1,3 @@
-""" Read meee~
-This is a list of lessons that the video "How to program in Godot - GDScript Tutorial" by Brackey
-has explained, transcribed personally by me for the conveniency of future learners. I'll be using 
-these kind of comments to divide the timestamps, and use '#' comments to explain every detail. 
-Do note that most of the syntaxes have their definition in Godot console, and you can read them by
-hovering your cursor on the specific syntax. Happy learning! (unfinished)
-"""
 #created by default. Necessary for the script to run and be used for your project.
 extends Node
 
